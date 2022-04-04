2803247a9e604814b5b415ce365b162acd61a1f7
so yeon <alicesysy@swu.ac.kr>
2022-04-04 19:51:48 +0900

    [Fix] 디자인시스템 폰트 수정 (#5)

--- Instagram/Instagram/Global/DesignSystem/Font/IDSFont.swift
+++ Instagram/Instagram/Global/DesignSystem/Font/IDSFont.swift
@@ -10,11 +10,7 @@ import UIKit
 public enum IDSFont {
     static let body1 = UIFont.systemFont(ofSize: 17, weight: .regular)
     static let body2 = UIFont.systemFont(ofSize: 16, weight: .regular)
-    static let body3 = UIFont.systemFont(ofSize: 16, weight: .regular)
-    static let body4 = UIFont.systemFont(ofSize: 15, weight: .regular)
-    static let body5 = UIFont.systemFont(ofSize: 14, weight: .regular)
-    static let body6 = UIFont.systemFont(ofSize: 14, weight: .regular)
-    static let body7 = UIFont.systemFont(ofSize: 13, weight: .regular)
-    static let body8 = UIFont.systemFont(ofSize: 12, weight: .regular)
-    static let body9 = UIFont.systemFont(ofSize: 10, weight: .regular)
+    static let body3 = UIFont.systemFont(ofSize: 15, weight: .regular)
+    static let body4 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let body5 = UIFont.systemFont(ofSize: 13, weight: .regular)
 }
