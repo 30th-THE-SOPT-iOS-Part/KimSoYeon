013da538c01108cadb9a6f66758b53df6f3631fe
so yeon <alicesysy@swu.ac.kr>
2022-04-25 21:03:05 +0900

    [Add] 에셋, 디자인시스템, extension 추가 (#11)

--- Instagram/Instagram/Global/DesignSystem/Font/IDSFont.swift
+++ Instagram/Instagram/Global/DesignSystem/Font/IDSFont.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 public enum IDSFont {
+    static let head1 = UIFont.systemFont(ofSize: 14, weight: .semibold)
+    static let head2 = UIFont.systemFont(ofSize: 12, weight: .semibold)
+    
     static let body1 = UIFont.systemFont(ofSize: 24, weight: .regular)
     static let body2 = UIFont.systemFont(ofSize: 14, weight: .regular)
     static let body3 = UIFont.systemFont(ofSize: 12, weight: .regular)
     
-    static let head1 = UIFont.systemFont(ofSize: 14, weight: .semibold)
-    static let head2 = UIFont.systemFont(ofSize: 12, weight: .semibold)
+    static let body4 = UIFont.systemFont(ofSize: 10, weight: .medium)
 }
--- Instagram/Instagram/Global/Extension/UILabel+.swift
+++ Instagram/Instagram/Global/Extension/UILabel+.swift
@@ -5,4 +5,14 @@
 //  Created by 소연 on 2022/04/25.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func setTextSpacingBy(value: Double) {
+        if let textString = self.text {
+            let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: value, range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
+}
