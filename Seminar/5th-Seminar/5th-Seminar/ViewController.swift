33cfbbdaf0b1f1a896a6836b6ae96fbcb72a2d50
so yeon <alicesysy@swu.ac.kr>
2022-05-28 15:02:14 +0900

    [Add] 7차 세미나 추가 (#15)

---
+++ Seminar/5th-Seminar/5th-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5th-Seminar
+//
+//  Created by 소연 on 2022/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
