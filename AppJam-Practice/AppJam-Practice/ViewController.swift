63b9f2cfed2984b89426072b1faa010402ba347f
so yeon <alicesysy@swu.ac.kr>
2022-06-20 23:52:49 +0900

    [Add] 프로젝트 추가

---
+++ AppJam-Practice/AppJam-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppJam-Practice
+//
+//  Created by 소연 on 2022/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
